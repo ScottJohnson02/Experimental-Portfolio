e492a8334c4b584016441c28d02983a054190554
Scott Johnson <scottyjohnson002@gmail.com>
2021-06-18 00:47:04 -0400

    slightly working version with additional javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,58 @@
-document.getElementById('test').style.color = "red";
+function closeWindow() {
+  document.getElementById("about-window").style.display = "none";
+}
+
+function openWindow() {
+  document.getElementById("about-window").style.display = "block";
+}
+
+function closeProjectWindow() {
+  document.getElementById("projects-window").style.display = "none";
+}
+
+function openProjectWindow() {
+  document.getElementById("projects-window").style.display = "block";
+}
+
+dragElement(document.getElementById("projects-window"));
+dragElement(document.getElementById("about-window"));
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
